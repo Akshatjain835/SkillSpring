c30a6e99b87414888e7f12919874f3795a495822
Yo-Joshwa <shivambaberwal123@gmail.com>
2025-07-08 22:04:15 +0530

    Build Courses Page

---
+++ client/src/pages/student/Course.jsx
@@ -0,0 +1,40 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
+import { Card, CardContent } from "@/components/ui/card";
+import React from "react";
+
+function Course() {
+  return (
+    <Card className="overflow-hidden rounded-lg dark:bg-gray-800 bg-white shadow-lg hover:shadow-2xl transform hover:scale-105 transition-all duration-300">
+      <div className="relative ">
+        <img
+          src=""
+          alt="course"
+          className="w-full h-36 object-cover rounded-t-lg"
+        />
+      </div>
+      <CardContent className="px-5 py-4 space-y-3">
+        <h1 className="hover:underline font-bold text-lg truncate">
+          NextJs complete course
+        </h1>
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-3">
+            <Avatar className="h-8 w-8">
+              <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+            <h1 className="font-medium text-sm">Roman Reigns</h1>
+          </div>
+          <Badge className="text-white px-2 py-1 bg-blue-600 text-xs rounded-full">
+            Advanced
+          </Badge>
+        </div>
+        <div className="text-lg font-bold">
+            <span>₹499</span>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default Course;

235fa25407a43fb747eae2c6a0d91d4fa9de8a3c
shreyansh749 <shreyanshjais49@gmail.com>
2025-07-20 11:48:37 +0530

    Updating classes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -44,7 +44,7 @@ function Navbar() {
     }
   }, [isSuccess]);
   return (
-    <div className="h-16 dark:bg-[#0A0A0A] bg-white border-b-gray-200 dark:border-b-gray-800 fixed top-0 left-0 right-0 duration-300 z-10">
+    <div className="h-16 dark:bg-[#020817] bg-white border-b dark:border-b-gray-800 fixed top-0 left-0 right-0 duration-300 z-10">
       {/* Desktop */}
       <div className="max-w-7xl mx-auto hidden justify-between items-center gap-10 h-full md:flex ">
         <div className="flex items-center gap-2">
@@ -107,7 +107,7 @@ function Navbar() {
           <SheetTrigger asChild>
             <Button
               size="icon"
-              className="rounded-full bg-gray-200 hover:bg-gray-300"
+              className="rounded-full hover:bg-gray-300"
               variant="outline"
             >
               <Menu />
--- client/src/layout/MainLayout.jsx
+++ client/src/layout/MainLayout.jsx
@@ -4,13 +4,13 @@ import { Outlet } from 'react-router-dom'
 
 function MainLayout() {
   return (
-    <div>
-        <Navbar />
-        <div>
-            <Outlet />
-        </div>
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <div className="flex-1 mt-16">
+        <Outlet />
+      </div>
     </div>
-  )
+  );
 }
 
 export default MainLayout

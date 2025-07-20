d0217893aab73deceea772a676c1f09cf0f6a379
shreyansh749 <shreyanshjais49@gmail.com>
2025-07-20 11:30:18 +0530

    Light/Dark mode is completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { appRoutes } from "./routes/appRoutes";
+import { ThemeProvider } from "./components/ThemeProvider";
 
 function App() {
   const router = createBrowserRouter(appRoutes);
 
   return (
     <main>
-      <RouterProvider router={router} />
+      <ThemeProvider>
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </main>
   );
 }

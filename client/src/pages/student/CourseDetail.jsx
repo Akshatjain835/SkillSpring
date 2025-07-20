f837f0a2e29856e71d75b98708b38300c4dbde28
shreyansh749 <shreyanshjais49@gmail.com>
2025-07-20 12:30:02 +0530

    Course Detail is updated

--- client/src/pages/student/CourseDetail.jsx
+++ client/src/pages/student/CourseDetail.jsx
@@ -84,7 +84,7 @@ const CourseDetail = () => {
         <div className="w-full lg:w-1/2 space-y-5">
           <h1 className="font-bold text-xl md:text-2xl">Description</h1>
           <p
-            className="text-sm bg-white text-black"
+            className="text-sm"
             dangerouslySetInnerHTML={{ __html: course?.description }}
           />
           <Card>

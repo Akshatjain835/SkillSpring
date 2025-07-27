04ec9ed1d397807e7b103549cb80f008883964da
Yo-Joshwa <shivambaberwal123@gmail.com>
2025-07-27 13:07:13 +0530

    logout query updated

--- client/src/features/api/authApi.js
+++ client/src/features/api/authApi.js
@@ -39,6 +39,7 @@ export const authApi = createApi({
       }),
       async onQueryStarted(_, {queryFulfilled, dispatch}) {
                 try { 
+                    await queryFulfilled;
                     dispatch(userLoggedOut());
                 } catch (error) {
                     console.log(error);
